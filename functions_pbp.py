e3f2bf82f507c414d6c46c2001a2daca4c21129d
Eduardo_Garcia <63433556+egarcia00@users.noreply.github.com>
2023-11-28 17:41:38 +0100

    Finally Creating a local app

--- functions_pbp.py
+++ functions_pbp.py
@@ -11,8 +11,9 @@ def get_play_by_play(GAME_ID,perspective='HomeScore'):
     play_by_play = play_by_play.get_data_frames()[0]
     play_by_play = play_by_play[['PERIOD','PCTIMESTRING','SCORE']]
     play_by_play = play_by_play.fillna(value=np.nan)
-    play_by_play['HomeScore'] = play_by_play['SCORE'].str.split('-',expand=True).fillna(value=np.nan)[0]
-    play_by_play['AwayScore'] = play_by_play['SCORE'].str.split('-',expand=True).fillna(value=np.nan)[1]
+    
+    play_by_play['HomeScore'] = play_by_play['SCORE'].str.split('-',expand=True)[0]#.fillna(value=np.nan)[0]
+    play_by_play['AwayScore'] = play_by_play['SCORE'].str.split('-',expand=True)[1]#.fillna(value=np.nan)[1]
     play_by_play['HomeScore'] = pd.to_numeric(play_by_play['HomeScore'])
     play_by_play['AwayScore'] = pd.to_numeric(play_by_play['AwayScore'])
     play_by_play['HomeDelta'] = play_by_play['HomeScore'] - play_by_play['AwayScore']
@@ -25,22 +26,72 @@ def get_play_by_play(GAME_ID,perspective='HomeScore'):
     periods = range(1, 5)
     times = pd.date_range("00:12:00", "00:00:00", freq="-1S").strftime('%M:%S').tolist()
     template_df = pd.DataFrame([(p, t) for p in periods for t in times], columns=['PERIOD', 'PCTIMESTRING'])
-
+    print(template_df.shape)
     # Merge with original data
     merged_df = pd.merge(template_df, original_df, 
                         left_on=['PERIOD', 'PCTIMESTRING'], 
                         right_on=['PERIOD', 'PCTIMESTRING'], 
                         how='left')
-
+    print(merged_df.shape)
 
     # Find max SCORE for each PERIOD & PCTIMESTRING combination
-    merged_df['HomeScore'] = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['HomeScore'].transform('max')
-    merged_df['AwayScore'] = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['AwayScore'].transform('max')
+    # merged_df['HomeScore'] = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['HomeScore'].transform('max')
+    # merged_df['AwayScore'] = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['AwayScore'].transform('max')
     # Fill null values with previous value
     merged_df['HomeScore'] = merged_df['HomeScore'].fillna(method='bfill')
     merged_df['AwayScore'] = merged_df['AwayScore'].fillna(method='bfill')
-    # fig, ax = plt.subplots()
-    # ax.plot(merged_df[perspective])
+    merged_df['HomeDelta'] = merged_df['HomeScore']-merged_df['AwayScore']
+    merged_df['TotalPoints'] = merged_df['HomeScore']+merged_df['AwayScore']
+    merged_df = merged_df[['PERIOD', 'PCTIMESTRING','HomeDelta','TotalPoints']]
     
+    # Find the rows with the highest TotalPoints for each unique combination
+    unique_combinations = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['TotalPoints'].idxmax()
+
+    # Use the above indices to select the corresponding rows
+    result_df = merged_df.loc[unique_combinations]
+
+    # Sort the result by index in ascending order
+    result_df.sort_index(ascending=True, inplace=True)
+
+    # Reset the index
+    result_df.reset_index(drop=True, inplace=True)
+
+    return result_df
+
+
+def create_home_delta_plot(df):
+    font = {'family': 'arial',
+        'color':  'darkblue',
+        'weight': 'normal',
+        'size': 14,
+        }
+    fig, ax = plt.subplots()
+    ax.plot(df['HomeDelta'])
+    ax.hlines(y=0, xmin=0, xmax=df.shape[0], colors='black')
+
+    for period in range(1, 5):
+        ax.vlines(x=(60 * 12 * period),
+                  ymin=df['HomeDelta'].min(),
+                  ymax=df['HomeDelta'].max(),
+                  colors='gray',
+                  ls='--'
+                  )
     
-    return merged_df
+    for period in range(5, (df['PERIOD'].max() + 1)):
+        ax.vlines(x=(60 * 5 * period),
+                  ymin=df['HomeDelta'].min() // 5,
+                  ymax=df['HomeDelta'].max(),
+                  colors='gray',
+                  ls='--'
+                  )
+    
+    y_ticks = np.arange(((df['HomeDelta'].min() // 5)) * 5, ((df['HomeDelta'].max() // 5) + 2) * 5, 5)
+    plt.yticks(y_ticks)
+    
+    x_labels = [(12 * 60 * 1), (12 * 60 * 2), (12 * 60 * 3), (12 * 60 * 4)]
+    plt.xticks(x_labels, labels=['Q1', 'Q2', 'Q3', 'Q4'])
+    
+    plt.xlabel('Period / time (s)', fontdict=font)
+    plt.ylabel('Home Delta', fontdict=font)
+
+    return fig
--- my_app.py
+++ my_app.py
@@ -1,41 +1,29 @@
-# import streamlit as st
-# import pandas as pd
-# import matplotlib as plt
-# from functions_pbp import get_play_by_play
-# from nba_api.stats.endpoints import playbyplayv2
-# import numpy as np
-
-# # Load your data (adjust the path to your actual data file)
-# nba_games_id = pd.read_csv('nba_games_id.csv')
-
-# # Streamlit app
-
-# st.title("NBA Play-by-Play Data Viewer 22222")
-
-# # Create a select box for matchups
-# matchups = nba_games_id['MATCHUP'].unique()
-
-# with st.form(key='my_form'):
-#     matchup=st.selectbox('Select Matchup.', list(nba_games_id['MATCHUP']))
-#     submit_button = st.form_submit_button(label='Submit')
-
-# if submit_button:
-#     game_id = nba_games_id['GAME_ID'].loc[nba_games_id['MATCHUP']==matchup]
-#     df = get_play_by_play(game_id)
-
-# st.line_chart(df['HomeScore'])
-
-import pandas as pd
-import pandas_datareader as web
-import datetime as dt
-import numpy as np
 import streamlit as st
-st.title('Crypto price over time.')
+import pandas as pd
+import matplotlib as plt
+from functions_pbp import get_play_by_play, create_home_delta_plot
+from nba_api.stats.endpoints import playbyplayv2
+import numpy as np
+
+# Load your data (adjust the path to your actual data file)
+nba_games_id = pd.read_csv('nba_games_id.csv',  
+                            dtype= {'	Unnamed: 0':str, 
+                            'GAME_ID':str,
+                            'MATCHUP':str})[["GAME_ID","MATCHUP"]]
+
+# Streamlit app
+
+st.title("NBA Play-by-Play Data Viewer")
+
+# Create a select box for matchups
+matchups = nba_games_id['MATCHUP'].unique()
+
 with st.form(key='my_form'):
-    crypto=st.selectbox('Select Cryptocurrency', ['BTC','ETH','XRP','BCH'])
-    start=st.date_input('Start')
-    end=st.date_input('End')
+    matchup=st.selectbox('Select Matchup.', list(nba_games_id['MATCHUP']))
     submit_button = st.form_submit_button(label='Submit')
+
 if submit_button:
-    df = web.DataReader(f'{crypto}-USD', 'yahoo', start, end)
-st.line_chart(df['Adj Close'])
+    game_id = nba_games_id['GAME_ID'].loc[nba_games_id['MATCHUP']==matchup]
+    df = get_play_by_play(game_id)
+    # Use Streamlit to create and display the plot
+    st.pyplot(create_home_delta_plot(df))
