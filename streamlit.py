e5d7a8124125db56e57865d1566594f398dd60c1
Eduardo_Garcia <63433556+egarcia00@users.noreply.github.com>
2023-11-28 14:07:57 +0100

    Deleting unnecessary selectbox directly plotting

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib as plt
+import matplotlib as plt
 from functions import get_play_by_play
 
 # Load your data (adjust the path to your actual data file)
@@ -12,7 +12,6 @@ st.title("NBA Play-by-Play Data Viewer")
 
 # Create a select box for matchups
 matchups = nba_games_id['MATCHUP'].unique()
-selected_matchup = st.selectbox("Select a Matchup", matchups)
 
 with st.form(key='my_form'):
     matchup=st.selectbox('Select Matchup.', list(nba_games_id['MATCHUP']))
@@ -20,6 +19,4 @@ with st.form(key='my_form'):
 
 if submit_button:
     game_id = nba_games_id['GAME_ID'].loc[nba_games_id['MATCHUP']==matchup]
-    fig = get_play_by_play(game_id)
-
-st.pyplot(fig)
+    get_play_by_play(game_id)
