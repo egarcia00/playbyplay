996207f6d03ba986ac927e948685f1a4bc8312fa
Eduardo_Garcia <63433556+egarcia00@users.noreply.github.com>
2024-05-17 12:30:59 +0200

    Add new file Homedescription.xlsx and modify functions_pbp.py, my_app.py, and sandbox_notebook.ipynb.

--- functions_pbp.py
+++ functions_pbp.py
@@ -1,64 +1,10 @@
-from nba_api.stats.endpoints import playbyplayv2
-from nba_api.stats.endpoints import leaguegamelog
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from datetime import datetime
-
-def get_play_by_play(GAME_ID,perspective='HomeScore'):
-    # Fetch the play-by-play data for the specified game
-    play_by_play = playbyplayv2.PlayByPlayV2(game_id=GAME_ID)
-
-    # Extract the data to a DataFrame
-    play_by_play = play_by_play.get_data_frames()[0]
-    play_by_play = play_by_play[['PERIOD','PCTIMESTRING','SCORE']]
-    play_by_play = play_by_play.fillna(value=np.nan)
-    
-    play_by_play['HomeScore'] = play_by_play['SCORE'].str.split('-',expand=True)[0]#.fillna(value=np.nan)[0]
-    play_by_play['AwayScore'] = play_by_play['SCORE'].str.split('-',expand=True)[1]#.fillna(value=np.nan)[1]
-    play_by_play['HomeScore'] = pd.to_numeric(play_by_play['HomeScore'])
-    play_by_play['AwayScore'] = pd.to_numeric(play_by_play['AwayScore'])
-    play_by_play['HomeDelta'] = play_by_play['HomeScore'] - play_by_play['AwayScore']
-    play_by_play['AwayDelta'] = -play_by_play['HomeDelta']
-    play_by_play['PCTIMESTRING']= play_by_play['PCTIMESTRING'].apply(lambda x: ':'.join(part.zfill(2) for part in x.split(':')))
-    
-    original_df = play_by_play[['PERIOD','PCTIMESTRING','HomeScore','AwayScore']]
-
-    # Create template DataFrame
-    periods = range(1, 5)
-    times = pd.date_range("00:12:00", "00:00:00", freq="-1S").strftime('%M:%S').tolist()
-    template_df = pd.DataFrame([(p, t) for p in periods for t in times], columns=['PERIOD', 'PCTIMESTRING'])
-    print(template_df.shape)
-    # Merge with original data
-    merged_df = pd.merge(template_df, original_df, 
-                        left_on=['PERIOD', 'PCTIMESTRING'], 
-                        right_on=['PERIOD', 'PCTIMESTRING'], 
-                        how='left')
-    print(merged_df.shape)
-
-    # Find max SCORE for each PERIOD & PCTIMESTRING combination
-    # merged_df['HomeScore'] = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['HomeScore'].transform('max')
-    # merged_df['AwayScore'] = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['AwayScore'].transform('max')
-    # Fill null values with previous value
-    merged_df['HomeScore'] = merged_df['HomeScore'].fillna(method='bfill')
-    merged_df['AwayScore'] = merged_df['AwayScore'].fillna(method='bfill')
-    merged_df['HomeDelta'] = merged_df['HomeScore']-merged_df['AwayScore']
-    merged_df['TotalPoints'] = merged_df['HomeScore']+merged_df['AwayScore']
-    merged_df = merged_df[['PERIOD', 'PCTIMESTRING','HomeDelta','TotalPoints']]
-    
-    # Find the rows with the highest TotalPoints for each unique combination
-    unique_combinations = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['TotalPoints'].idxmax()
-
-    # Use the above indices to select the corresponding rows
-    result_df = merged_df.loc[unique_combinations]
-
-    # Sort the result by index in ascending order
-    result_df.sort_index(ascending=True, inplace=True)
-
-    # Reset the index
-    result_df.reset_index(drop=True, inplace=True)
-
-    return result_df
+from nba_api.stats.endpoints import boxscoretraditionalv2
+from nba_api.stats.endpoints import playbyplayv2
+from nba_api.stats.endpoints import leaguegamelog
 
 def create_home_delta_plot(df):
     
@@ -95,7 +41,7 @@ def create_home_delta_plot(df):
 
 def get_all_nba_games(season):
     # Initialize an empty list to store game data
-    all_games = []
+    all_games = [] 
 
     # Specify the range of seasons you want to cover
     # For example, from the 1946-47 season to the 2020-21 season
@@ -112,6 +58,158 @@ def get_all_nba_games(season):
 
         # Combine all seasons into a single DataFrame
         all_games_df = pd.concat(all_games, ignore_index=True)
-    filtered_df = all_games_df[~all_games_df["MATCHUP"].str.contains("@")]
-    filtered_df = filtered_df.reset_index(drop=True)
-    return filtered_df
+        all_games_df = all_games_df[['GAME_ID',	'GAME_DATE', 'MATCHUP',	'TEAM_ID']]
+        all_games_df = all_games_df.sort_values(by=['GAME_DATE','GAME_ID'])
+
+        # Identifying the home and away teams.
+        home_team_row = all_games_df[~all_games_df['MATCHUP'].str.contains('@')]
+        away_team_row = all_games_df[all_games_df['MATCHUP'].str.contains('@')]
+
+        # Constructing the target DataFrame.
+        target_state_data = {
+            'GAME_ID': home_team_row['GAME_ID'].values,
+            'GAME_DATE': home_team_row['GAME_DATE'].values,
+            'MATCHUP': home_team_row['MATCHUP'].str.replace('@', 'vs.').str.strip(),
+            'HOMETEAM_ID': home_team_row['TEAM_ID'].values,
+            'AWAYTEAM_ID': away_team_row['TEAM_ID'].values
+        }
+
+        # Creating the DataFrame for the target state.
+        target_df = pd.DataFrame(target_state_data)
+
+    return target_df
+
+def get_starting_lineup(GAME_ID, TEAM_ID):
+    game_id =  GAME_ID
+    boxscore = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=game_id)
+    boxscore_df = boxscore.get_data_frames()[0]
+    starting_lineup = list(boxscore_df.loc[boxscore_df['TEAM_ID']==TEAM_ID][0:5]["PLAYER_ID"])
+    return starting_lineup
+
+def get_lead_timeline(GAME_ID):
+    play_by_play = playbyplayv2.PlayByPlayV2(game_id=GAME_ID)
+
+    play_by_play = play_by_play.get_data_frames()[0]
+    play_by_play = play_by_play[['PERIOD','PCTIMESTRING','SCORE']]
+    play_by_play = play_by_play.fillna(value=np.nan)
+    
+    play_by_play['HomeScore'] = play_by_play['SCORE'].str.split('-',expand=True)[0]#.fillna(value=np.nan)[0]
+    play_by_play['AwayScore'] = play_by_play['SCORE'].str.split('-',expand=True)[1]#.fillna(value=np.nan)[1]
+    play_by_play['HomeScore'] = pd.to_numeric(play_by_play['HomeScore'])
+    play_by_play['AwayScore'] = pd.to_numeric(play_by_play['AwayScore'])
+
+    play_by_play['PCTIMESTRING']= play_by_play['PCTIMESTRING'].apply(lambda x: ':'.join(part.zfill(2) for part in x.split(':')))
+    
+    original_df = play_by_play[['PERIOD','PCTIMESTRING','HomeScore','AwayScore']]
+
+    # Create template DataFrame
+    periods = range(1, 5)
+    times = pd.date_range("00:12:00", "00:00:00", freq="-1S").strftime('%M:%S').tolist()
+    template_df = pd.DataFrame([(p, t) for p in periods for t in times], columns=['PERIOD', 'PCTIMESTRING'])
+
+    # Merge with original data
+    merged_df = pd.merge(template_df, original_df, 
+                        left_on=['PERIOD', 'PCTIMESTRING'], 
+                        right_on=['PERIOD', 'PCTIMESTRING'], 
+                        how='left')
+
+    # Find max SCORE for each PERIOD & PCTIMESTRING combination
+    # Fill null values with previous value
+    merged_df['HomeScore'] = merged_df['HomeScore'].fillna(method='bfill')
+    merged_df['AwayScore'] = merged_df['AwayScore'].fillna(method='bfill')
+    
+    merged_df['HomeDelta'] = merged_df['HomeScore']-merged_df['AwayScore']
+    merged_df['AwayDelta'] = -merged_df['HomeDelta']
+
+    merged_df['HomeDif'] = merged_df['HomeDelta'].diff() 
+    merged_df['HomeDif'].iloc[0] = merged_df['HomeDelta'].iloc[0]
+
+    merged_df['AwayDif'] = merged_df['AwayDelta'].diff() 
+    merged_df['AwayDif'].iloc[0] = merged_df['AwayDelta'].iloc[0]
+
+    merged_df['TotalPoints'] = merged_df['HomeScore']+merged_df['AwayScore']
+    merged_df = merged_df[['PERIOD', 'PCTIMESTRING','HomeDelta','HomeDif','AwayDelta','AwayDif','TotalPoints']]
+    
+    # Find the rows with the highest TotalPoints for each unique combination
+    unique_combinations = merged_df.groupby(['PERIOD', 'PCTIMESTRING'])['TotalPoints'].idxmax()
+
+    # Use the above indices to select the corresponding rows
+    result_df = merged_df.loc[unique_combinations]
+
+    # Sort the result by index in ascending order
+    result_df.sort_index(ascending=True, inplace=True)
+
+    # Reset the index
+    result_df.reset_index(drop=True, inplace=True)
+
+    return result_df
+
+def get_lineup_log(GAME_ID, HOMETEAM_ID, AWAYTEAM_ID):
+    HOME_STARTING_LINEUP = get_starting_lineup(GAME_ID, HOMETEAM_ID)
+    HOME_STARTING_LINEUP.insert(0,HOMETEAM_ID)
+    HOME_STARTING_LINEUP.insert(1,1)
+    HOME_STARTING_LINEUP.insert(2,"12:00")
+
+    AWAY_STARTING_LINEUP = get_starting_lineup(GAME_ID, AWAYTEAM_ID)
+    AWAY_STARTING_LINEUP.insert(0,AWAYTEAM_ID)
+    AWAY_STARTING_LINEUP.insert(1,1)
+    AWAY_STARTING_LINEUP.insert(2,"12:00")
+
+    boxscore = boxscoretraditionalv2.BoxScoreTraditionalV2(game_id=GAME_ID)
+    boxscore_df = boxscore.get_data_frames()[0]
+
+    playbyplay = playbyplayv2.PlayByPlayV2(game_id=GAME_ID)
+    playbyplay_df = playbyplay.get_data_frames()[0]
+
+    DESCRIPTION_STRING = ["HOMEDESCRIPTION", "VISITORDESCRIPTION"]
+    TEAMS_ID = [HOMETEAM_ID, AWAYTEAM_ID]
+    STARTING_LINEUP = [HOME_STARTING_LINEUP, AWAY_STARTING_LINEUP]
+
+    lineup_df = pd.DataFrame(columns=['TEAM_ID','PERIOD','PCTIMESTRING','PLAYER1','PLAYER2','PLAYER3','PLAYER4','PLAYER5'])
+
+    for j, team_description in enumerate(DESCRIPTION_STRING):
+        lineup_df.loc[len(lineup_df)] = STARTING_LINEUP[j]
+        substitutions_log = playbyplay_df[playbyplay_df[team_description].str.contains('sub', case=False)&
+                                        playbyplay_df[team_description].notna()
+                                        ]
+        substitutions_log['TEAM_ID'] = TEAMS_ID[j]
+
+        substitutions_log = substitutions_log[['TEAM_ID','PERIOD','PCTIMESTRING',team_description,
+                                            'PLAYER1_ID','PLAYER1_NAME','PLAYER2_ID','PLAYER2_NAME']]
+        substitutions_log = substitutions_log.reset_index(drop=True)
+
+
+    # substitutions_log
+        for i in range(0,len(substitutions_log)):
+            PERIOD_SUB = substitutions_log['PERIOD'].values[i]
+            PCTIMESTRING_SUB = substitutions_log['PCTIMESTRING'].values[i]
+            player_out = substitutions_log['PLAYER1_ID'].values[i]
+            player_in  = substitutions_log['PLAYER2_ID'].values[i]
+            
+            previous_lineup = list(lineup_df.loc[len(lineup_df)-1].values)
+            new_lineup = previous_lineup
+            new_lineup[0] = TEAMS_ID[j]
+            new_lineup[1] = PERIOD_SUB
+            new_lineup[2] = PCTIMESTRING_SUB
+            new_lineup = [player_in if x == player_out else x for x in new_lineup]
+            lineup_df.loc[len(lineup_df)] = new_lineup
+
+            idx = lineup_df.groupby(['TEAM_ID','PERIOD', 'PCTIMESTRING'])['PLAYER1'].idxmax()
+            clean_lineup_df = lineup_df.loc[idx].sort_index().reset_index(drop=True)
+            clean_lineup_df['PCTIMESTRING']= clean_lineup_df['PCTIMESTRING'].apply(lambda x: ':'.join(part.zfill(2) for part in x.split(':')))
+
+    # clean_lineup_df = clean_lineup_df.sort_values(by=['PERIOD','PCTIMESTRING'], ascending=[True, False]).reset_index(drop=True)
+    return clean_lineup_df
+
+# def lead_timeline_lineup(GAME_ID, HOMETEAM_ID, AWAYTEAM_ID):
+
+            def lead_timeline_lineup(GAME_ID, HOMETEAM_ID, AWAYTEAM_ID):
+                lineup_df = get_lineup_log(GAME_ID, HOMETEAM_ID, AWAYTEAM_ID)
+                lead_timeline = get_lead_timeline(GAME_ID)
+                
+                merged_df = pd.merge(lead_timeline, lineup_df, 
+                                    left_on=['PERIOD', 'PCTIMESTRING'], 
+                                    right_on=['PERIOD', 'PCTIMESTRING'], 
+                                    how='left')
+                
+                return merged_df
--- my_app.py
+++ my_app.py
@@ -8,39 +8,35 @@ from nba_api.stats.endpoints import playbyplayv2
 import numpy as np
 from datetime import datetime
 
+
 st.title("NBA IN GAME LEAD TRACKER\n (1996-2023)")
-tab1, tab2 = st.tabs(['README', 'LEAD TRACKER'])
 
-with tab1:
-    st.header("README")
+st.header("LEAD TRACKER")
 
-with tab2:
-    st.header("LEAD TRACKER")
-    
-    with st.sidebar:
-        st.header("Filters:")
-        year_list = list(reversed(range(1996, 2024)))
-        selected_year = st.selectbox("Select a Year", year_list)
-        df_season_games = get_all_nba_games(selected_year)
+with st.sidebar:
+    st.header("Filters:")
+    year_list = list(reversed(range(1996, 2024)))
+    selected_year = st.selectbox("Select a Year", year_list)
+    df_season_games = get_all_nba_games(selected_year)
 
-        if selected_year:
-            min_season_dates = datetime.strptime(df_season_games['GAME_DATE'].min(),'%Y-%m-%d').date()
-            max_season_dates = datetime.strptime(df_season_games['GAME_DATE'].max(),'%Y-%m-%d').date()
-            selected_date = st.date_input("Select a date", 
-                                            value=min_season_dates, 
-                                            min_value=min_season_dates, 
-                                            max_value=max_season_dates, 
-                                            format="YYYY/MM/DD"
-                                            )
-            if selected_date:
-                # daily_matches = df_season_games.loc[df_season_games['GAME_DATE']==selected_date]['MATCHUP'].unique()
-                # daily_matches = df_season_games['MATCHUP'].unique()
-                matchup = st.selectbox('Select Matchup', list(df_season_games.loc[df_season_games['GAME_DATE']==str(selected_date)]['MATCHUP']))
-                game_id = str(df_season_games.loc[  (df_season_games['MATCHUP']==matchup) & 
-                                                    (df_season_games['GAME_DATE']==str(selected_date))
-                                                ]['GAME_ID'].tolist()[0])
-                
+    if selected_year:
+        min_season_dates = datetime.strptime(df_season_games['GAME_DATE'].min(),'%Y-%m-%d').date()
+        max_season_dates = datetime.strptime(df_season_games['GAME_DATE'].max(),'%Y-%m-%d').date()
+        selected_date = st.date_input("Select a date", 
+                                        value=min_season_dates, 
+                                        min_value=min_season_dates, 
+                                        max_value=max_season_dates, 
+                                        format="YYYY/MM/DD"
+                                        )
+        if selected_date:
+            # daily_matches = df_season_games.loc[df_season_games['GAME_DATE']==selected_date]['MATCHUP'].unique()
+            # daily_matches = df_season_games['MATCHUP'].unique()
+            matchup = st.selectbox('Select Matchup', list(df_season_games.loc[df_season_games['GAME_DATE']==str(selected_date)]['MATCHUP']))
+            game_id = str(df_season_games.loc[  (df_season_games['MATCHUP']==matchup) & 
+                                                (df_season_games['GAME_DATE']==str(selected_date))
+                                            ]['GAME_ID'].tolist()[0])
+            
 
-    df = get_play_by_play(game_id)
-    # Use Streamlit to create and display the plot
-    st.pyplot(create_home_delta_plot(df))
+df = get_play_by_play(game_id)
+# Use Streamlit to create and display the plot
+st.pyplot(create_home_delta_plot(df))
